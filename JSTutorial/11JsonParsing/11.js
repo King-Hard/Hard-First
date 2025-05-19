9243cf442cbe455227f1012e01864ed02b49bd9e
Hard <hahahah63gd.sgsu@gmail.com>
2025-05-19 18:10:50 +0800

    .

---
+++ JSTutorial/11JsonParsing/11.js
@@ -0,0 +1,49 @@
+container = [];
+
+while(true){
+
+    console.log("1. Add Product");
+    console.log("2. View all Product");
+    console.log();
+
+    let choices = Number(prompt("Enter choice: "));
+    console.log();
+
+    if(choices === 1){
+        let firstName = String(prompt("First name: "));
+        let lastName = String(prompt("Last name: "));
+        let age = Number(prompt("How old are you?: "));
+        let eSports = String(prompt("E/Sports: "));
+        console.log();
+
+        let person = {
+            firstName: firstName,
+            lastName: lastName,
+            age: age,
+            eSports: eSports
+        };
+
+        container.push(person);
+        console.log("Prodduct succesfully added.");
+        console.log();
+    }
+
+    else if(choices === 2){
+        for(let i in container){
+            console.log(`First Name  : ${container[i].firstName}.`);
+            console.log(`Last Name   : ${container[i].lastName}.`);
+            console.log(`Age         : ${container[i].age}.`);
+            console.log(`eSports     : ${container[i].eSports}.`);
+            console.log();
+        }
+    }
+
+    else{
+        console.log("Invalid Input!");
+        console.log();
+    }
+    
+
+}
+
+
--- JSTutorial/11JsonParsing/Readme.md
+++ JSTutorial/11JsonParsing/Readme.md
@@ -1,2 +1,8 @@
 # Json Parsing
-1, 
+1, json - json natin para sayang container na nay lamang datas like kunyari variable natin person ang value nya is firstName, lastname, and age. 
+
+2, json structure - sa constract ng json natin meron tayong variable and then equal sign lang tayo saka curly braces sa loob non, don nakalagayu mga datas natin such as name, age, hobbbies, ans son. Sa json natin pwede rin tayo magalagay ng data na anaka array.
+
+3, json read/write - para ma call natin yung value na nasa loob ng person varibale natin na value gagamit lang tayo ng tuldok . like may person tayo and last name na value ngayon console.log lang then variable ng json natin tapos tuldok then yung name nung key natin peron.firstname
+
+6, json array - sa pag call natin ng value kada laman ng array json natin need lang natin ng variable ng json[i].variable nung key natin which is don sa may hawwak ng data.
